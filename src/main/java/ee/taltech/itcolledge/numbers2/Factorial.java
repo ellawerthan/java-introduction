5a0e3bb1e2dad6dcada7391b5ab6019ee1963d04
ellawerthan <ellawerthan@gmail.com>
2019-02-16 19:24:32 +0200

    functions-math-2 solutions

--- src/main/java/ee/taltech/itcolledge/numbers2/Factorial.java
+++ src/main/java/ee/taltech/itcolledge/numbers2/Factorial.java
@@ -4,15 +4,17 @@ public class Factorial {
 
     public static long factorial(int n) {
         //todo rewrite with tertiary if statement (using ? and :) (colon, not a smiley)
-        if (1 > n) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
+        return (1 > n ? 1 : n * factorial(n - 1));
     }
 
     public static void main(String[] args) {
         //todo call factorial function with inputs 0, 2, 5, 10, 15, 20
         // what happens on factorial 21?
+        factorial(0);
+        factorial(2);
+        factorial(5);
+        factorial(10);
+        factorial(15);
+        factorial(20);
     }
 }
--- src/main/java/ee/taltech/itcolledge/numbers2/NumberCalculator2.java
+++ src/main/java/ee/taltech/itcolledge/numbers2/NumberCalculator2.java
@@ -2,18 +2,14 @@ package ee.taltech.itcolledge.numbers2;
 
 public class NumberCalculator2 {
 
-    public static int sum(int x1, int x2) {
-        return 0;
-    }
-
-    public static int sum(int x1, int x2, int x3) {
-        return 0;
-    }
-
-    public static int sum(int x1, int x2, int x3, int x4) {
-        return 0;
-    }
-
     //todo create method that takes almost infinite amount of variables
     // hint: varargs
+
+    public static int sum(int ... a) {
+        int sum = 0;
+        for(int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
 }
